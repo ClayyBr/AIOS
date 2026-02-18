0dba03f16ea5619300305cd5571b042408ab1946
ClayyBr <cleison-braga@outlook.com>
2026-02-18 03:01:00 -0300

    chore: remove lp-creator from backup/main

--- squads/lp-creator/output/fintech-teste02/next.config.js
+++
@@ -1,1 +0,0 @@
-/** @type {import('next').NextConfig} */\nconst nextConfig = {\n  images: {\n    remotePatterns: [\n      {\n        protocol: 'https',\n        hostname: 'images.unsplash.com',\n        port: '',\n        pathname: '/**',\n      },\n    ],\n  },\n};\n\nmodule.exports = nextConfig;\n
